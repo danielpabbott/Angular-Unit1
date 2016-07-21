c0cd16115b009c145dd340c752aaf69d2b50a517
Daniel Abbott <danielpabbott@gmail.com>
2016-07-21 16:37:23 -0600

    create add article functionality

--- reddit-clone/app.js
+++ reddit-clone/app.js
@@ -1,4 +1,21 @@
 angular.module('redditClone', [])
   .controller('controllerName', function($scope) {
-    $scope.name = 'daniel';
+    $scope.postForm = false;
+    $scope.addPost = function() {
+      $scope.posts.push({
+        title: $scope.title,
+        author: $scope.author,
+        description: $scope.description,
+        image_url: $scope.image_url,
+        date: new Date(),
+        rating: 0
+      });
+    }
+    $scope.upVote = function() {
+      this.post.rating += 1;
+    };
+    $scope.downVote = function() {
+      this.post.rating -= 1;
+    };
+    $scope.posts = [{title: 'The First Article', author: 'Daniel Abbott', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://placekitten.com/200/200', date: new Date(), rating: 0}];
   })
