fc6dc16a96c5f4f331a3d6a3801e6361fca787d4
Daniel Abbott <danielpabbott@gmail.com>
2016-07-27 11:25:58 -0600

    clear forms on submission

--- reddit-clone/app.js
+++ reddit-clone/app.js
@@ -13,13 +13,19 @@ angular.module('redditClone', [])
         rating: 0,
         comments: []
       });
+      $scope.title = "";
+      $scope.author = "";
+      $scope.description = "";
+      $scope.image_url = "";
     }
     $scope.addComment = function(form) {
       console.log(form.commAuthor)
       $scope.posts[this.$index].comments.push({
         commAuthor: form.commAuthor,
         comment: form.comment
-      })
+      });
+      form.commAuthor="";
+      form.comment="";
     }
     $scope.upVote = function() {
       this.post.rating += 1;
@@ -27,6 +33,6 @@ angular.module('redditClone', [])
     $scope.downVote = function() {
       this.post.rating -= 1;
     };
-    $scope.posts = [{title: 'The First Article', author: 'Daniel Abbott', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://placekitten.com/200/200', date: new Date(), rating: 0, comments: [{commAuthor: 'Daniel', comment: 'neato neato neato'}]},
-    {title: 'The Second Article', author: 'Bradford', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://placekitten.com/200/200', date: new Date(), rating: 0, comments: [{commAuthor: 'Bradford', comment: 'this site sux'}]}];
+    $scope.posts = [{title: 'The First Article', author: 'Daniel Abbott', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://placekitten.com/200/200', date: new Date(), rating: 2, comments: [{commAuthor: 'Daniel', comment: 'neato neato neato'}]},
+    {title: 'The Second Article', author: 'Bradford', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image_url: 'https://placekitten.com/200/200', date: new Date(), rating: -1, comments: [{commAuthor: 'Bradford', comment: 'this site sux'}]}];
   })
